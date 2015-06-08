a76cba309f060b1d3fba9d3e7b2eb76e85c8a596
Alexandr Kalenyuk <kalenyuk.alex@gmail.com>
2015-06-08 12:40:08 +0300

    implement full page caching

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,11 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
--- src/AppBundle/Controller/StudentsController.php
+++ src/AppBundle/Controller/StudentsController.php
@@ -5,19 +5,24 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Student;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-
+/**
+ * Class StudentsController
+ * @Route("/students")
+ */
 class StudentsController extends Controller
 {
     /**
      * @param Student $student
-     * @Route("/students/detail/{path}", name="detail")
-     * @Template
+     * @Route("/detail/{path}", name="detail")
+     * @Cache(maxage="900", expires="15 minutes", public=true)
+     * @Template("AppBundle:Students:detail.html.twig", vars={"student"})
      * @return array
      */
     public function detailAction(Student $student)
     {
-        return ['student' => $student];
+
     }
 }
