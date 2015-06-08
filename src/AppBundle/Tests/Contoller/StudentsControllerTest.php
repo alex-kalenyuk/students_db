5fbc50e59d31ac7dd4126aa42e58105c87c19021
Alexandr Kalenyuk <kalenyuk.alex@gmail.com>
2015-06-08 13:10:07 +0300

    add test for Student controller

---
+++ src/AppBundle/Tests/Contoller/StudentsControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+
+class StudentsControllerTest extends WebTestCase
+{
+    const FIFTEEN_MIN_IN_SEC = 900;
+    /**
+     * @var Client
+     */
+    public $client;
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testDetailActionCheckCache()
+    {
+        $this->client->request('GET', '/students/detail/firstname_lastname');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(self::FIFTEEN_MIN_IN_SEC, $this->client->getResponse()->getMaxAge());
+        $this->assertEquals(new \DateTime('15 minutes'), $this->client->getResponse()->getExpires());
+    }
+
+    public function testDetailActionCheckUniquePath()
+    {
+        $this->client->request('GET', '/students/detail/firstname_lastname_1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+}
